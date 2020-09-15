0f17f7c21dc0079a191bc086abf2de7198c479e2
victorbonomi16 <bononikis@gmail.com>
2020-09-15 16:59:13 -0300

    feat: exercicio 1 concluido

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+minhaTupla = ("corsa","celta","palio","gol","jetta")
+print("Tupla de carros:", type(minhaTupla))
+
+minhaTupla["corsa"] = "celta"
+print("Tupla apos ser modificada:", minhaTupla)
+#o erro acontece  pois a classe tuple é  imutável, e impossibilita  qualquer mudança. 
