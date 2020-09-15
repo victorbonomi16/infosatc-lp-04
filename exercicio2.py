8c1c5bdfdb7d33e6ed8fca30892152025cc988a2
victorbonomi16 <bononikis@gmail.com>
2020-09-15 17:01:51 -0300

    feat: exercicio 2 concluido

---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+meuDicionario = {
+ "Julia"      :  17 ,  
+ "Guilherme"  :  16 ,
+ "Nathan"     :  16 ,
+ "Kamila"     :  17 ,
+ "Igor   "    :  15 ,
+  }
+for x in meuDicionario:
+    print(x + ":",meuDicionario[x]) 
